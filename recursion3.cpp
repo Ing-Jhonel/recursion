430f17e85f6ebdb72391aa527a49943c3b40d99f
Jhonel <jgapazap@unjbg.edu.pe>
2024-09-18 23:47:01 -0500

    Ejercicio de recursion 3

---
+++ recursion3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int maxValor(double a[], int, double);
+
+int main(){
+	
+	int cant;
+	cout << "Indique la cantidad de elementos a ingresar: ";
+	cin >> cant;
+
+	double arreglo[cant];
+	
+	for(int i=0; i<cant; i++){
+		cout << i+1 << ". elemento: ";
+		cin >> arreglo[i];
+	}
+	
+	double mayor = arreglo[0];
+	double r = maxValor(arreglo, cant, mayor);
+	
+	cout << "El maximo elemento ingresado es: " << r << endl;
+	
+	return 0;
+}
+
+int maxValor(double a[], int n, double x){
+	
+	if(n==0){
+		return x;
+	}
+	
+	if(a[n]>x){
+		return maxValor(a, n-1, a[n]);
+	}
+	
+	return maxValor(a, n-1, x);
+	
+}
